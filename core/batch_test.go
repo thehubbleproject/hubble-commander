9f6295c524ae4ce9031d8bd05495f1f761d5abd3
vaibhavchellani <vaibhavchellani223@gmail.com>
2021-02-01 13:45:06 +0530

    fix: testcase

--- core/batch_test.go
+++ core/batch_test.go
@@ -13,7 +13,7 @@ func TestTransferBody(t *testing.T) {
 	expectedOutput := "3a53dc4890241dbe03e486e785761577d1c369548f6b09aa38017828dcdf5c2707857e73108d077c5b7ef89540d6493f70d940f1763a9d34c9d98418a39d28ac02bb0e4743a7d0586711ee3dd6311256579ab7abcd53c9c76f040bfde4d6d6e90000000000000000000000000000000000000000000000000000000000000000000000000000000100010000"
 
 	// bytes32 accountRoot
-	accountRoot := "3a53dc4890241dbe03e486e785761577d1c369548f6b09aa38017828dcdf5c27"
+	accountRoot := "0x3a53dc4890241dbe03e486e785761577d1c369548f6b09aa38017828dcdf5c27"
 	// uint256[2] calldata signatures
 	signatures := []string{
 		"3402053321874964899321528271743396700217057178612185975187363512030360053932",
@@ -26,8 +26,8 @@ func TestTransferBody(t *testing.T) {
 	require.Nil(t, err)
 
 	var signature [2]*big.Int
-	signature[0] = StringToBigInt(signatures[0])
-	signature[1] = StringToBigInt(signatures[1])
+	signature[0] = StringToDecimalBigInt(signatures[0])
+	signature[1] = StringToDecimalBigInt(signatures[1])
 
 	txssBz, err := hex.DecodeString(txss)
 	require.Nil(t, err)
@@ -46,4 +46,11 @@ func TestTransferBody(t *testing.T) {
 	require.Nil(t, err)
 
 	require.Equal(t, expectedOutput, hex.EncodeToString(data), "error output doesnt match")
+}
+
+// StringToDecimalBigInt takes in a string and returns the corresponding big int
+func StringToDecimalBigInt(s string) *big.Int {
+	t := big.NewInt(0)
+	t.SetString(s, 10)
+	return t
 }
