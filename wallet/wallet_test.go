bc4d8291c4734f1ee092a9aa21ab84880f44fd17
Chih Cheng Liang <chihchengliang@gmail.com>
2021-02-09 12:32:25 +0800

    debug private key

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,9 +5,22 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/crypto"
 	blswallet "github.com/kilic/bn254/bls"
 	"github.com/stretchr/testify/require"
 )
+
+func TestPrivateKey(t *testing.T) {
+	domainBytes, _ := hex.DecodeString("1234ABCD")
+	// keyMaterial, _ := hex.DecodeString("5566")
+	// key := crypto.Keccak256Hash(keyMaterial).Bytes()
+	key, _ := hex.DecodeString("0bbb61c6ddb95fa018b3cbd40fe7abe2861c54040646dcc76011013e8352ce1a")
+	fmt.Printf("%x", key)
+	wallet, err := SecretToWallet(key, crypto.Keccak256Hash(domainBytes))
+	require.NoError(t, err)
+	pubkeyBytes, _ := hex.DecodeString("1d66806e962cbb8c9ef907c2b17996b5d908ee93d86cddc3c3f0401f8ba1dfba24030326130d84e3d2019b0bbc5bf724b0ec84ec4f341586d891cc49f0424fa3061ac5817571d009b58a7b3f2e05ed9d0c4b8f4f741e3fed47217561785c23f11118f4af592b7f48163427ca489eee044594ee1148b0b8e8eb7e35e8a7c13137")
+	require.Equal(t, fmt.Sprintf("%x", pubkeyBytes), fmt.Sprintf("%x", wallet.signer.Account.Public.ToBytes()))
+}
 
 func TestSignAndVerify(t *testing.T) {
 	wallet, err := NewWallet(DefaultDomain)
